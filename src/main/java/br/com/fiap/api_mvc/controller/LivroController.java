b4ff93c6eafdd50c1e3afab987ffaca404a43132
Luiz Gustavo S. Real <lgsreal2@gmail.com>
2024-08-15 09:19:48 -0300

    Add Validation

--- src/main/java/br/com/fiap/api_mvc/controller/LivroController.java
+++ src/main/java/br/com/fiap/api_mvc/controller/LivroController.java
@@ -8,8 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+// localhost:8080/livros
 @RequestMapping(value = "/livros")
 public class LivroController {
     @Autowired
@@ -29,5 +31,34 @@ public class LivroController {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(listaLivros, HttpStatus.OK);
+    }
+    // /livros?id=1
+    @GetMapping("/{id}")
+    public ResponseEntity<Livro> read(@PathVariable Long id) {
+        Optional<Livro> livro = livroRepository.findById(id);
+        if (livro.isPresent()) {
+            return new ResponseEntity<>(livro.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    // /livros/1
+    @PutMapping("/{id}")
+    public ResponseEntity<Livro> update(@PathVariable Long id, @RequestBody Livro livro) {
+        Optional<Livro> livroPersistido = livroRepository.findById(id);
+        if (livroPersistido.isPresent()) {
+            livro.setId(id);
+            return new ResponseEntity<>(livroRepository.save(livro), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Livro> delete(@PathVariable Long id) {
+        Optional<Livro> livro = livroRepository.findById(id);
+        if (livro.isPresent()) {
+            livroRepository.delete(livro.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/br/com/fiap/api_mvc/model/Livro.java
+++ src/main/java/br/com/fiap/api_mvc/model/Livro.java
@@ -1,6 +1,7 @@
 package br.com.fiap.api_mvc.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 @Entity
@@ -10,13 +11,18 @@ public class Livro {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "nome")
+    @NotBlank(message = "O nome é obrigatório")
     private String nome;
     @Column(name = "preco")
+    @DecimalMin(value = "0.99", message = "O valor mínimo deve ser 0.99")
     private BigDecimal preco;
     @Column(name = "categoria")
     @Enumerated(EnumType.STRING)
+    @NotNull(message = "A categoria do livro é obrigatória")
     private Categoria categoria;
     @Column(name = "autoria")
+    @Size(min = 4, message = "O nome do autor deve ter no mínimo 3 caracteres")
+    @NotBlank(message = "O nome do(a) autor(a) não deve estar em branco")
     private String autoria;
 
     public Long getId() {
